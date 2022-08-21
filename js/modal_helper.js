1b5cae9b3802bce09fc411299f9875f9653e50a2
seven <i.tech.github@gmail.com>
2022-08-21 13:11:16 +0300

    init

---
+++ js/modal_helper.js
@@ -0,0 +1,59 @@
+// modal 1 ************************
+const modal_container_1 = document.getElementById("modal_container_1");
+const open_1 = document.getElementById("read_more_btn_1");
+open_1.addEventListener("click", () => {
+  modal_container_1.classList.add("show");
+});
+const close_1 = document.getElementById("modal_close_btn_1");
+const close_X_1 = document.getElementById("modal_close_btn_X_1");
+close_1.addEventListener("click", () => {
+  modal_container_1.classList.remove("show");
+});
+close_X_1.addEventListener("click", () => {
+  modal_container_1.classList.remove("show");
+});
+
+// modal 2 ************************
+const modal_container_2 = document.getElementById("modal_container_2");
+const open_2 = document.getElementById("read_more_btn_2");
+open_2.addEventListener("click", () => {
+  modal_container_2.classList.add("show");
+});
+const close_2 = document.getElementById("modal_close_btn_2");
+const close_X_2 = document.getElementById("modal_close_btn_X_2");
+close_2.addEventListener("click", () => {
+  modal_container_2.classList.remove("show");
+});
+close_X_2.addEventListener("click", () => {
+  modal_container_2.classList.remove("show");
+});
+
+// modal 3 ************************
+const modal_container_3 = document.getElementById("modal_container_3");
+const open_3 = document.getElementById("read_more_btn_3");
+open_3.addEventListener("click", () => {
+  modal_container_3.classList.add("show");
+});
+const close_3 = document.getElementById("modal_close_btn_3");
+const close_X_3 = document.getElementById("modal_close_btn_X_3");
+close_3.addEventListener("click", () => {
+  modal_container_3.classList.remove("show");
+});
+close_X_3.addEventListener("click", () => {
+  modal_container_3.classList.remove("show");
+});
+
+// modal 4 ************************
+const modal_container_4 = document.getElementById("modal_container_4");
+const open_4 = document.getElementById("read_more_btn_4");
+open_4.addEventListener("click", () => {
+  modal_container_4.classList.add("show");
+});
+const close_4 = document.getElementById("modal_close_btn_4");
+const close_X_4 = document.getElementById("modal_close_btn_X_4");
+close_4.addEventListener("click", () => {
+  modal_container_4.classList.remove("show");
+});
+close_X_4.addEventListener("click", () => {
+  modal_container_4.classList.remove("show");
+});
